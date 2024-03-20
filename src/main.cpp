8a978548a5a5b4e81af79fd0a5f6fe2acfd72de7
nategalang <nagalang@up.edu.ph>
2024-03-20 14:13:34 +0800

    modified code for child node

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,15 @@
 #include <painlessMesh.h>
 
 /*Mesh Details*/
-#define   WIFI_CHANNEL    1 //Check the access point on your router for the channel - 6 is not the same for everyone
+#define   WIFI_CHANNEL    6 //Check the access point on your router for the channel - 6 is not the same for everyone
 #define   MESH_PREFIX     "whateveryouwant"
 #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
 
 /****** WiFi Connection Details *******/
-#define   STATION_SSID     "Jarvis"
-#define   STATION_PASSWORD "0n3_Voy@ger!"
-#define   BRIDGE_NODE
+#define   STATION_SSID     "CARE_407"
+#define   STATION_PASSWORD "nec_c@re"
+// #define   BRIDGE_NODE
 #define   HOSTNAME  "MQTT_Bridge"
 
 /******* MQTT Broker Connection Details *******/
@@ -134,7 +134,7 @@ void setup() {
   userScheduler.addTask( taskSendMessage );
   userScheduler.addTask( taskPublishMQTT );
   taskSendMessage.enable();
-  taskPublishMQTT.enable();
+  // taskPublishMQTT.enable();
 
   #ifdef BRIDGE_NODE
   mesh.stationManual(STATION_SSID, STATION_PASSWORD);
@@ -164,7 +164,8 @@ void receivedCallback( const uint32_t &from, const String &msg ) {
 
 void sendMessage() {
   String msg = "Random number: " + String(random(1,20));
-  mesh.sendBroadcast( msg );
+  uint32_t target = 1973942425;
+  Serial.println(mesh.sendSingle(target, msg));
   taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
 }
 
