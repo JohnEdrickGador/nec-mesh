dcaa8dfcef6426d4fc5d0c14913aefb63a3681fa
nategalang <nagalang@up.edu.ph>
2024-04-21 22:43:41 +0800

    added re-election algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <painlessMesh.h>
 #include <WiFi.h>
 #include <map>
+#include <SPI.h>
+#include <SD.h>
 
-#define   WIFI_CHANNEL    11 //Check the access point on your router for the channel - 6 is not the same for everyone
+#define   WIFI_CHANNEL    9 //Check the access point on your router for the channel - 6 is not the same for everyone
 #define   MESH_PREFIX     "test"
 #define   MESH_PASSWORD   "password"
 #define   MESH_PORT       5555
@@ -16,23 +18,22 @@
 void receivedCallback( const uint32_t &from, const String &msg );
 void newConnectionCallback(uint32_t nodeId);
 void changedConnectionCallback();
-void sink_node_election();
-void broadcast_rssi();
-void connect_to_wifi();
-void broadcast_status();
-void send_message();
+void sinkNodeElection();
+void broadcastRSSI();
+void connectToWifi();
+void writeFile(fs::FS &fs, const char * path, const char * message);
+void sendMessage();
 
-Task taskBroadcastRSSI (TASK_SECOND * 10, TASK_FOREVER, &broadcast_rssi);
-Task taskSendMessage( TASK_MINUTE * 1 , TASK_FOREVER, &send_message );
-Task taskBroadcastStatus(TASK_SECOND * 30 , TASK_FOREVER, &broadcast_status );
+Task taskBroadcastRSSI (TASK_SECOND * 10, TASK_FOREVER, &broadcastRSSI);
+Task taskSinkNodeElection (TASK_MINUTE * 2, TASK_FOREVER, &sinkNodeElection );
 
 painlessMesh  mesh;
 Scheduler userScheduler;
 
 int my_rssi = 0;
 int mesh_size = 4;
+String nodeRSSIString = "";
 std::map<uint32_t, int> nodeRSSIMap;
-int target_idx = 0;
 uint32_t target = 0;
 bool sne_done = false;
 bool isConnected = false;
@@ -41,12 +42,42 @@ void setup() {
   Serial.begin(115200);
   pinMode(10, OUTPUT);
 
-  WiFi.begin(STATION_SSID, STATION_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {}
-  Serial.println("Connected to WiFi!");
-  my_rssi = WiFi.RSSI();
-  WiFi.disconnect();
-  Serial.println(my_rssi);
+  while (!SD.begin(0)) {
+    Serial.println("Card Mount Failed");
+  }
+
+  File file = SD.open("/NodeID_RSSI1.txt");
+  if(!file) {
+    WiFi.begin(STATION_SSID, STATION_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {}
+    Serial.println("Connected to WiFi!");
+    my_rssi = WiFi.RSSI();
+    WiFi.disconnect();
+    Serial.println(my_rssi);
+  }
+  else {
+    sne_done = true;
+    String numberString = "";
+    int nodeID = 0;
+    int RSSI = 0;
+    while (file.available()) {
+      char c = file.read();
+      if (c == ':') {
+        nodeID = numberString.toInt();
+        Serial.println(nodeID);
+        numberString = "";
+      } else if (c == ',') {
+        RSSI = numberString.toInt();
+        Serial.println(RSSI);
+        numberString = "";
+        nodeRSSIMap.insert({nodeID, RSSI});
+      } 
+      else {
+        numberString = numberString + c;
+      }
+    }
+  }
+  file.close();
 
   mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
 
@@ -56,19 +87,21 @@ void setup() {
   mesh.onChangedConnections(&changedConnectionCallback);
 
   userScheduler.addTask( taskBroadcastRSSI );
-  userScheduler.addTask( taskBroadcastStatus );
-  userScheduler.addTask( taskSendMessage );
+  userScheduler.addTask( taskSinkNodeElection );
 
   while(mesh.getNodeList().size() + 1 != mesh_size) {
     mesh.update();
   }
   Serial.println("All nodes connected!");
 
-  sink_node_election();
+  sinkNodeElection();
 }
 
 void loop() {
   mesh.update();
+  if(!taskSinkNodeElection.isEnabled()){
+    taskSinkNodeElection.enableDelayed(120000);
+  }
 }
 
 void receivedCallback( const uint32_t &from, const String &msg ) {
@@ -87,26 +120,21 @@ void changedConnectionCallback() {
   Serial.printf("Changed connections\n");
 }
 
-void broadcast_rssi() {
+void broadcastRSSI() {
   mesh.sendBroadcast(String(my_rssi));
   Serial.print("RSSI Broadcasted: ");
   Serial.println(my_rssi);
 }
 
-void broadcast_status() {
-  mesh.sendBroadcast("I am still connected!");
-}
-
-void connect_to_wifi() {
+void connectToWifi() {
   mesh.stationManual(STATION_SSID, STATION_PASSWORD);
   mesh.setHostname(HOSTNAME);
-  if(WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected to wifi!");
-    isConnected = true;
-  }
+  Serial.println("Connected to wifi!");
+  isConnected = true;
+  mesh.sendBroadcast("I am connected to the access point!");
 }
 
-void send_message() {
+void sendMessage() {
   if(mesh.sendSingle(target, "Hello from the child node!")) {
     Serial.println("Message sent to the sink node!");
   }
@@ -115,9 +143,23 @@ void send_message() {
   }
 }
 
-void sink_node_election() {
-  nodeRSSIMap.clear();
-  
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+void sinkNodeElection() {
   nodeRSSIMap.insert({mesh.getNodeId(), my_rssi});
   
   while (nodeRSSIMap.size() != mesh_size) {
@@ -125,19 +167,28 @@ void sink_node_election() {
     mesh.update();
   }
 
+  if(!sne_done) {
+    taskBroadcastRSSI.enableIfNot();
+    taskBroadcastRSSI.setIterations(1);
+  }
+
   int maxRSSI = INT_MIN; // Initialize to the smallest possible integer
 
   // Iterate through the map
   for (const auto& pair : nodeRSSIMap) {
+    nodeRSSIString = nodeRSSIString + String(pair.first) + ":" + String(pair.second) + ",";
+
     // Check if the current RSSI is greater than the maximum RSSI found so far
     if (pair.second > maxRSSI) {
       // Update the maximum RSSI and corresponding node ID
-      maxRSSI = pair.second;
-      target = pair.first;
+      if(pair.first == mesh.getNodeId() || (pair.first != mesh.getNodeId() && mesh.isConnected(pair.first))) {
+        maxRSSI = pair.second;
+        target = pair.first;
+      }
     }
     else if (pair.second == maxRSSI) {
       if (pair.first < target) {
-        target = pair.first;
+        if(pair.first == mesh.getNodeId() || (pair.first != mesh.getNodeId() && mesh.isConnected(pair.first))){target = pair.first;}
       }
     }
   }
@@ -146,16 +197,26 @@ void sink_node_election() {
   Serial.println("Sink node election done!");
   Serial.println("Sink node is " + String(target));
 
-  taskBroadcastRSSI.setIterations(1);
+  File file = SD.open("/NodeID_RSSI1.txt");
+
+  if(!file) {
+    Serial.println("File doesn't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/NodeID_RSSI1.txt", nodeRSSIString.c_str());
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+
+  file.close();
 
   if(mesh.getNodeId() == target) {
-    connect_to_wifi();
+    connectToWifi();
     digitalWrite(10, HIGH);
-    taskBroadcastStatus.enable();
   }
 
   else {
-    taskSendMessage.enableDelayed(30000);
+    sendMessage();
     mesh.sendBroadcast("My sink node is " + String(target));
     digitalWrite(10, LOW);
   }
