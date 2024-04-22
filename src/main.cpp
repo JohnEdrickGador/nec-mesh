e40b88b63ae03ea36a0654793cad3b04a3ab3aed
nategalang <nagalang@up.edu.ph>
2024-04-22 16:30:11 +0800

    added sd card

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ void writeFile(fs::FS &fs, const char * path, const char * message);
 void sendMessage();
 
 Task taskBroadcastRSSI (TASK_SECOND * 10, TASK_FOREVER, &broadcastRSSI);
-Task taskSinkNodeElection (TASK_SECOND * 30, TASK_FOREVER, &sinkNodeElection );
+Task taskSinkNodeElection (TASK_SECOND * 60, TASK_FOREVER, &sinkNodeElection );
 
 painlessMesh  mesh;
 Scheduler userScheduler;
@@ -46,7 +46,7 @@ void setup() {
     Serial.println("Card Mount Failed");
   }
 
-  File file = SD.open("/NodeID_RSSI3.txt");
+  File file = SD.open("/NodeID_RSSI4.txt");
   if(!file) {
     WiFi.begin(STATION_SSID, STATION_PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {}
@@ -177,6 +177,7 @@ void sinkNodeElection() {
   // Iterate through the map
   for (const auto& pair : nodeRSSIMap) {
     nodeRSSIString = nodeRSSIString + String(pair.first) + ":" + String(pair.second) + ",";
+    Serial.println(nodeRSSIString);
 
     // Check if the current RSSI is greater than the maximum RSSI found so far
     if (pair.second > maxRSSI) {
@@ -197,18 +198,20 @@ void sinkNodeElection() {
   Serial.println("Sink node election done!");
   Serial.println("Sink node is " + String(target));
 
-  File file = SD.open("/NodeID_RSSI3.txt");
+  File file = SD.open("/NodeID_RSSI4.txt");
 
   if(!file) {
     Serial.println("File doesn't exist");
     Serial.println("Creating file...");
-    writeFile(SD, "/NodeID_RSSI3.txt", nodeRSSIString.c_str());
+    writeFile(SD, "/NodeID_RSSI4.txt", nodeRSSIString.c_str());
   }
   else {
     Serial.println("File already exists");  
   }
 
   file.close();
+
+  nodeRSSIString = "";
 
   if(mesh.getNodeId() == target) {
     connectToWifi();
