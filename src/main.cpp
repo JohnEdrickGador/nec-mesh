25bbf0b59b5fb3b14d1309b6a28bfd01f5b13892
JohnEdrickGador <jmgador@up.edu.ph>
2024-04-27 19:29:30 +0800

    add: initialize libraries and added mesh details

--- src/main.cpp
+++ src/main.cpp
@@ -18,3 +18,23 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
+/*** Initialize Libraries ***/
+painlessMesh mesh;
+Scheduler userScheduler;
+HTTPClient http;
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+SensirionI2CSen5x sen5x;
+Adafruit_INA219 ina219;
+Adafruit_SGP30 sgp;
+
+/*** Mesh Details ***/
+#define   WIFI_CHANNEL    6 //Check the access point on your router for the channel - 6 is not the same for everyone
+#define   MESH_PREFIX     "NEC_4TH_FLOOR"
+#define   MESH_PASSWORD   "CARE_OFFICE"
+#define   MESH_PORT       5555
+#define   MESH_SIZE       3
+
+/*** Access Point Credentials ***/
+#define   STATION_SSID          "CARE_407"
+#define   STATION_PASSWORD      "nec_c@re"
